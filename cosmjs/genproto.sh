f82dfd6cace96df872760260f0a1a4d415a51915
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-02-09 01:38:49 +0900

    compile proto as camelCase

--- cosmjs/genproto.sh
+++ cosmjs/genproto.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf ./client/src/types/generated/*
+
 proto_dirs=$(find ./proto/checkers -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 dir_test=$(find . \( -path ./gogoproto -prune -o -path ./cosmos -prune -o -path ./google -prune \) -o -name '*.proto' -print)
 #echo "$dir_test"
@@ -11,7 +13,7 @@ for d in $proto_dirs; do
       --plugin="./node_modules/.bin/protoc-gen-ts_proto" \
       --ts_proto_out="./client/src/types/generated" \
       --proto_path="./proto" \
-      --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages" \
+      --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages,snakeToCamel=true" \
       "$file"
   done < <(find "$d" -maxdepth 1 -name '*.proto' -print0)
 done
